07c3919d11d398ed9a9f6a4cb21c1b167876bf2f
Akmal76 <akmalramadhan76@gmail.com>
2023-09-21 15:48:43 +0700

    Menyelesaikan lab 4

---
+++ LCS.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.*;
+
+public class LCS {
+    private static InputReader in;
+    private static PrintWriter out;
+
+    public static int lcs (int n, int m) {
+        
+        return -1;
+    }
+
+    // Template
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        in = new InputReader(inputStream);
+        OutputStream outputStream = System.out;
+        out = new PrintWriter(outputStream);
+        
+        
+
+        // don't forget to close the output
+        out.close();
+    }
+    // taken from https://codeforces.com/submissions/Petr
+    // together with PrintWriter, these input-output (IO) is much faster than the usual Scanner(System.in) and System.out
+    // please use these classes to avoid your fast algorithm gets Time Limit Exceeded caused by slow input-output (IO)
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong(){
+            return Long.parseLong(next());
+        }
+
+    }
+}
